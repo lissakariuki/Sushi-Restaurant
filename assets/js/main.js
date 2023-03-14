dae7546c4f91b9d9a75deeb6ce9fcafb951a87e6
kooya3 <elyeestatua@gmail.com>
2023-03-15 01:19:30 +0300

    specified menu to be removed in mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,10 +20,15 @@ if(navClose){
     })
 }
 
-
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
-
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 
