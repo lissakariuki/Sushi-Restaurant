b8bab7a540c37e3af14056bdec04d1c28047278e
kooya3 <elyeestatua@gmail.com>
2023-03-15 12:15:19 +0300

    implemented js functionality to scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,6 +40,13 @@ const scrollHeader = () =>{
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
