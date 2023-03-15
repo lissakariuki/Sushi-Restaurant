912471eeba87250611c8ef59197d826a04e8be1e
kooya3 <elyeestatua@gmail.com>
2023-03-15 15:21:20 +0300

    final commit after implementing scrollreveal functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,3 +102,15 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.home__img, .newsletter__container, .footer__logo, .footer__description, .footer__content, footer__info`)
+sr.reveal(`.home__data`, {origin: 'bottom'})
+sr.reveal(`.about__data, recently__data`, {origin: 'left'})
+sr.reveal(`.about__img, recently__img`, {origin: 'right'})
+sr.reveal(`.popular__card`, {interval: 115})
